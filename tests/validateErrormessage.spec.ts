22a44b45942944a929129a1d0a3ecde2f379b68f
Penchal <penchal2013@gmail.com>
2026-01-04 17:02:57 +0530

    updated the test description for validateErrormessage test

--- tests/validateErrormessage.spec.ts
+++ tests/validateErrormessage.spec.ts
@@ -1,9 +1,5 @@
 import{expect,test} from '@playwright/test'
-<<<<<<< HEAD
-test('test dropdown field',async ({page})=>{
-=======
 test('@regression test error message while logging in',async ({page})=>{
->>>>>>> ef88c29 (updated validateErrormessagetest)
 await page.goto("https://demowebshop.tricentis.com/login");
 await page.locator("#Email").fill("penchal@gmail.com");
 await page.locator("#Password").fill("penchal@");
@@ -12,5 +8,4 @@ await page.locator('[value="Log in"]').click();
 let errormessage:string=await page.locator('.validation-summary-errors').textContent()??"";
 console.log(errormessage);
 page.pause();
-
 })
