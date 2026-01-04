22b5a083d6dfca8bb0f2572982e36dc51e0b6c89
Penchal <penchal2013@gmail.com>
2026-01-04 13:09:17 +0530

    A error validate test added

---
+++ tests/validateErrormessage.spec.ts
@@ -0,0 +1,12 @@
+import{expect,test} from '@playwright/test'
+test('test dropdown field',async ({page})=>{
+await page.goto("https://demowebshop.tricentis.com/login");
+await page.locator("#Email").fill("penchal@gmail.com");
+await page.locator("#Password").fill("penchal@");
+await page.locator('[value="Log in"]').click();
+//await page.waitForTimeout(1000);
+let errormessage:string=await page.locator('.validation-summary-errors').textContent()??"";
+console.log(errormessage);
+page.pause();
+
+})
